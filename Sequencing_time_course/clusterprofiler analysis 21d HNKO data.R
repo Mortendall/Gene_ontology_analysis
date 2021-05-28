c878d01241d3c7b64a23290cdd458162488b40b4
Morten Dall <mortendall1988@gmail.com>
2021-05-28 17:59:39 +0200

    expanded GO analysis to MF and CC. Added some heatmap analysis

--- Sequencing_time_course/clusterprofiler analysis 21d HNKO data.R
+++ Sequencing_time_course/clusterprofiler analysis 21d HNKO data.R
@@ -8,6 +8,7 @@ library(limma)
 library(here)
 library(tidyverse)
 library(UpSetR)
+library(clusterProfiler)
 
 expressions <- read_excel("R/Re-run RNA seq set/21d HNKO effect RNAseq.xlsx")
 expressions_significant <- expressions %>%
@@ -140,10 +141,11 @@ cpm_matrix <- openxlsx::read.xlsx(here("Sequencing_time_course/cpmData sorted by
 
 #extract 3d only genes
 overlap_genes_3d <- as.data.frame(edgeR_sig[[1]]) %>%
-  dplyr::filter(!edgeR_sig$KO_at_3d %in% edgeR_sig$KO_at_6d &
-                  !edgeR_sig$KO_at_3d %in% edgeR_sig$KO_at_12d &
-                  !edgeR_sig$KO_at_3d %in% edgeR_sig$KO_at_21d
+  dplyr::filter(!edgeR_sig[[1]] %in% edgeR_sig[[2]] &
+                  !edgeR_sig[[1]] %in% edgeR_sig[[3]] &
+                  !edgeR_sig[[1]] %in% edgeR_sig[[4]]
   )
+  
 overlap_genes_3d <- overlap_genes_3d %>% 
   dplyr::filter(!is.na(overlap_genes_3d))
 colnames(overlap_genes_3d) <- "Symbol"
@@ -164,13 +166,14 @@ goResults_overlap_3d <- enrichGO(gene = overlap_entrez$ENTREZID,
 
 #repeat for 21d
 overlap_genes_21d <- as.data.frame(edgeR_sig[[4]]) %>%
-  dplyr::filter(!edgeR_sig$KO_at_21d %in% edgeR_sig$KO_at_3d &
-                  !edgeR_sig$KO_at_21d %in% edgeR_sig$KO_at_12d &
-                  !edgeR_sig$KO_at_21d %in% edgeR_sig$KO_at_6d
+  dplyr::filter(!edgeR_sig[[4]] %in% edgeR_sig[[2]] &
+                  !edgeR_sig[[4]] %in% edgeR_sig[[3]] &
+                  !edgeR_sig[[4]] %in% edgeR_sig[[1]]
   )
 overlap_genes_21d <- overlap_genes_21d %>% 
   dplyr::filter(!is.na(overlap_genes_21d))
 colnames(overlap_genes_21d) <- "Symbol"
+
 
 overlap_entrez <- bitr(overlap_genes_21d$Symbol, 
                        fromType = "SYMBOL", 
@@ -185,4 +188,114 @@ goResults_overlap_21d <- enrichGO(gene = overlap_entrez$ENTREZID,
                                  ont = "BP")
 enrichplot::dotplot(goResults_overlap_21d)+ggtitle("Genotype effect - Day 21")
 
-#Day 6
+#Day 3 significance test
+genes_3d <- as.data.frame(edgeR_sig[[1]])
+genes_3d <- genes_3d %>% 
+  dplyr::filter(!is.na(genes_3d))
+colnames(genes_3d) <- "Symbol"
+
+day3_entrez <- bitr(genes_3d$Symbol, 
+                       fromType = "SYMBOL", 
+                       toType = "ENTREZID", 
+                       OrgDb = "org.Mm.eg.db",
+                       drop = T)
+
+
+goResults_3d<- enrichGO(gene = day3_entrez$ENTREZID,
+                                  universe = overlap_bg$ENTREZID,
+                                  OrgDb = org.Mm.eg.db,
+                                  ont = "BP")
+enrichplot::dotplot(goResults_3d)+ggtitle("Genotype effect - Day 3")
+goResults_3d <- setReadable(goResults_3d, keyType = "ENTREZID", OrgDb = org.Mm.eg.db)
+
+
+goResults_3d_CC<- enrichGO(gene = day3_entrez$ENTREZID,
+                        universe = overlap_bg$ENTREZID,
+                        OrgDb = org.Mm.eg.db,
+                        ont = "CC")
+enrichplot::dotplot(goResults_3d_CC)+ggtitle("Genotype effect - Day 3")
+#day 21 significance test
+genes_21d <- as.data.frame(edgeR_sig[[4]])
+genes_21d <- genes_21d %>% 
+  dplyr::filter(!is.na(genes_21d))
+colnames(genes_21d) <- "Symbol"
+
+day21_entrez <- bitr(genes_21d$Symbol, 
+                    fromType = "SYMBOL", 
+                    toType = "ENTREZID", 
+                    OrgDb = "org.Mm.eg.db",
+                    drop = T)
+
+
+goResults_21d<- enrichGO(gene = day21_entrez$ENTREZID,
+                        universe = overlap_bg$ENTREZID,
+                        OrgDb = org.Mm.eg.db,
+                        ont = "BP")
+enrichplot::dotplot(goResults_21d)+ggtitle("Genotype effect - Day 21")
+goResults_21d <- setReadable(goResults_21d, keyType = "ENTREZID", OrgDb = org.Mm.eg.db)
+enrichplot::cnetplot(goResults_21d)
+
+goResults_21d_CC<- enrichGO(gene = day21_entrez$ENTREZID,
+                           universe = overlap_bg$ENTREZID,
+                           OrgDb = org.Mm.eg.db,
+                           ont = "CC")
+enrichplot::dotplot(goResults_21d_CC)+ggtitle("Genotype effect - Day 21")
+
+goResults_21d_MF<- enrichGO(gene = day21_entrez$ENTREZID,
+                            universe = overlap_bg$ENTREZID,
+                            OrgDb = org.Mm.eg.db,
+                            ont = "MF")
+enrichplot::dotplot(goResults_21d_MF)+ggtitle("Genotype effect - Day 21")
+goResults_21d_MF <- setReadable(goResults_21d_MF, keyType = "ENTREZID", OrgDb = org.Mm.eg.db)
+enrichplot::cnetplot(goResults_21d_MF)
+#heatmap of calcium ion binding proteins
+calcium_binding_proteins <- goResults_21d_MF@result[2,]$geneID
+Proteinlist_table <- read.table(text = calcium_binding_proteins, sep = "/") 
+Proteinlist_table <- t(Proteinlist_table)
+cpm_matrix <- openxlsx::read.xlsx(here::here("Sequencing_time_course/cpm_matrix.xlsx"))
+setup <- openxlsx::read.xlsx(here::here("Sequencing_time_course/setup.xlsx"))
+
+cpm_calcium <- cpm_matrix %>% 
+  dplyr::filter(SYMBOL %in% Proteinlist_table)
+
+setup <- setup %>% 
+  dplyr::mutate(Group = paste(Genotype, Time, sep = "_"))
+setup <- setup %>% 
+  dplyr::arrange(Time, desc(Genotype))
+setup$ID <- as.character(setup$ID)
+colnames(cpm_calcium)<-as.character(colnames(cpm_calcium))
+setup <- setup %>% 
+  dplyr::filter(ID %in% colnames(cpm_calcium))
+
+cpm_calcium_key <- cpm_calcium %>% 
+  dplyr::select(ENSEMBL, SYMBOL)
+cpm_calcium <- cpm_calcium %>% 
+  dplyr::select(-ENSEMBL, -SYMBOL)
+
+cpm_calcium <- cpm_calcium %>% 
+  dplyr::select(setup$ID)
+cpm_calcium <- as.matrix(cpm_calcium)
+rownames(cpm_calcium)<-cpm_calcium_key$SYMBOL
+key <- as.data.frame(setup$Group)
+colnames(key)<-"Group"
+rownames(key) <- setup$ID
+key$Group <- factor(key$Group, c("WT_3","KO_3","WT_6","KO_6","WT_12","KO_12","WT_21","KO_21"))
+
+calcium_hm<- pheatmap::pheatmap(cpm_calcium,
+                            treeheight_col = 0,
+                            treeheight_row = 0,
+                            scale = "row",
+                            legend = T,
+                            na_col = "white",
+                            Colv = NA,
+                            na.rm = T,
+                            cluster_cols = F,
+                            fontsize_row = 5,
+                            fontsize_col = 8,
+                            cellwidth = 6,
+                            cellheight = 4,
+                            annotation_col = key,
+                            show_colnames = F,
+                            show_rownames = F,
+                            main = "Oxidation-reduction process"
+)
